cc83fc057a6b52ca0a7ecdd9394f290917158ef5
luvkushmina <luvkushmina@gmail.com>
2026-02-10 08:18:04 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -205,7 +205,8 @@ React.useEffect(() => {
               PRAYASS Video Browser
             </h1>
             <img
-              src="https://github.com/luvkushmina/project-images/blob/43011a56af31cea250d46e4389b6d1b4eb1a4240/Prayas%20folder/ChatGPT%20Image%20Feb%2010%2C%202026%2C%2008_09_24%20AM.png"
+              src="https://raw.githubusercontent.com/luvkushmina/project-images/43011a56af31cea250d46e4389b6d1b4eb1a4240/Prayas%20folder/ChatGPT%20Image%20Feb%2010%2C%202026%2C%2008_09_24%20AM.png
+"
               alt="Folder structure of PRAYASS"
               className="rounded-xl shadow-lg w-full max-w-2xl mx-auto my-6"
             />
